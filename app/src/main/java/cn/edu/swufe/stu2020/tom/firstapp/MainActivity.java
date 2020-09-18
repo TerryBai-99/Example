dde38889a421f62c9763540079f2243826efdb2c
TerryBai-99 <2089354738@qq.com>
2020-09-18 15:53:43 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/swufe/stu2020/tom/firstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package cn.edu.swufe.stu2020.tom.firstapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+}
